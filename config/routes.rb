4e68ec00b10056cc053646a9d3c34371d9a2c228
Patrick Miller <pjmiller823@gmail.com>
2017-05-05 16:36:02 -0400

    green test for should get new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/character_classes'          => 'character_classes#index'
   get '/character_class'            => 'character_classes#show'
-  get '/character_class_new_url'    => 'character_classes#new'
+  get '/character_class_new'    => 'character_classes#new'
 end
